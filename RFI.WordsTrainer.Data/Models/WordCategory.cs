3d67d18d51e3fb00f740a2a1cd947a48a341669a
Filip Roman <Filip.Roman@sberbankcz.cz>
2022-04-05 19:13:22 +0200

    Add data access project

---
+++ RFI.WordsTrainer.Data/Models/WordCategory.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RFI.WordsTrainer.Data.Models
+{
+    public class WordCategory
+    {
+        [Key]
+        public int? Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+    }
+}
---
+++ RFI.WordsTrainer.Data/WordsTrainerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RFI.WordsTrainer.Data.Models;
+
+namespace RFI.WordsTrainer.Data
+{
+    public class WordsTrainerDbContext : DbContext
+    {
+        public DbSet<WordCategory>? WordCategories { get; set; }
+
+        public WordsTrainerDbContext(DbContextOptions<WordsTrainerDbContext> options)
+            : base(options)
+        { }
+    }
+}
