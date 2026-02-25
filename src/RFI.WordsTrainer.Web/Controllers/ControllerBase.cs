6ae981b28cc0789709079620b07d10ddf235e99c
Roman Filip <dicky@email.cz>
2026-02-25 13:40:41 +0100

    Add WordsController

---
+++ src/RFI.WordsTrainer.Web/Controllers/ControllerBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RFI.WordsTrainer.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
+{
+}
---
+++ src/RFI.WordsTrainer.Web/Controllers/WordsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RFI.WordsTrainer.Domain.Entities;
+using RFI.WordsTrainer.Domain.Enums;
+
+namespace RFI.WordsTrainer.Web.Controllers;
+
+public class WordsController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<WordTranslation> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index =>
+            WordTranslation.Create(
+                Word.Create($"Word {index}", Language.Cs),
+                Word.Create($"Translation {index}", Language.En)
+            )
+        ).ToArray();
+    }
+}
